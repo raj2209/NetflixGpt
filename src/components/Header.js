d62ab57d8694bec87c9ac9a12516d1e31cc7a738
raj2209 <rajeshwari0422@gamil.com>
2025-08-18 22:27:50 +0530

    firebase changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ const Header = () => {
   return (
     <div className="absolute w-full bg-gradient-to-b from-black to-transparent">
         <img src={LOGO_URL} alt='logo' className="w-44 mx-32 p-2" />
+        <h1>Hello</h1>
     </div>
   )
 }
